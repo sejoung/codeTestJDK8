29ec55f470427c9c7942644ac0ee556b16d0cca4
sejoung kim <sejoung@gmail.com>
2018-10-22 11:05:16 +0900

    싱글턴 테스트
    싱글턴 테스트

--- src/main/java/com/github/sejoung/codetest/singleton/SingletonTest.java
+++ src/main/java/com/github/sejoung/codetest/singleton/SingletonTest.java
@@ -7,66 +7,51 @@ import java.io.ObjectOutputStream;
 
 public class SingletonTest {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("1======================");
-        System.out.println(OldSingleton.NAME);
-        System.out.println("2======================");
-        OldSingleton oldSingleton1 = OldSingleton.INSTANCE;
-        System.out.println("3======================");
-        OldSingleton oldSingleton2 = OldSingleton.INSTANCE;
-        System.out.println("4======================");
-        System.out.println(serialize(oldSingleton1) == serialize(oldSingleton2));
+    public static void main(String[] args) {
+        
+        try {
+            System.out.println("1======================");
+            System.out.println(OldSingleton.NAME);
+            System.out.println("2======================");
+            OldSingleton oldSingleton1 = OldSingleton.INSTANCE;
+            System.out.println("3======================");
+            OldSingleton oldSingleton2 = OldSingleton.INSTANCE;
+            System.out.println("4======================");
+            System.out.println(serializeAndUnSerialize(oldSingleton1) == serializeAndUnSerialize(oldSingleton2));
 
-        System.out.println("1======================");
-        System.out.println(LazySingleton.NAME);
-        System.out.println("2======================");
-        LazySingleton lazySingleton1 = LazySingleton.getInstance();
-        System.out.println("3======================");
-        LazySingleton lazySingleton2 = LazySingleton.getInstance();
-        System.out.println("4======================");
-        System.out.println(serialize2(lazySingleton1) == serialize2(lazySingleton2));
+            System.out.println("1======================");
+            System.out.println(LazySingleton.NAME);
+            System.out.println("2======================");
+            LazySingleton lazySingleton1 = LazySingleton.getInstance();
+            System.out.println("3======================");
+            LazySingleton lazySingleton2 = LazySingleton.getInstance();
+            System.out.println("4======================");
+            System.out.println(serializeAndUnSerialize(lazySingleton1) == serializeAndUnSerialize(lazySingleton2));
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      
     }
 
-    public static OldSingleton serialize(OldSingleton o) throws Exception {
+    public static <T> T serializeAndUnSerialize(T o) throws Exception {
+        
         byte[] serialized;
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
                 oos.writeObject(o);
-                // serializedMember -> 직렬화된 member 객체
                 serialized = baos.toByteArray();
             }
         }
-        OldSingleton member = null;
+        
+        T unSerialized = null;
         try (ByteArrayInputStream bais = new ByteArrayInputStream(serialized)) {
             try (ObjectInputStream ois = new ObjectInputStream(bais)) {
-                // 역직렬화된 Member 객체를 읽어온다.
-                Object objectMember = ois.readObject();
-                member = (OldSingleton) objectMember;
+                Object object = ois.readObject();
+                unSerialized = (T) object;
             }
         }
-        return member;
-    }
-    
-    
-    public static LazySingleton serialize2(LazySingleton o) throws Exception {
-        byte[] serialized;
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
-                oos.writeObject(o);
-                // serializedMember -> 직렬화된 member 객체
-                serialized = baos.toByteArray();
-            }
-        }
-        LazySingleton member = null;
-        try (ByteArrayInputStream bais = new ByteArrayInputStream(serialized)) {
-            try (ObjectInputStream ois = new ObjectInputStream(bais)) {
-                // 역직렬화된 Member 객체를 읽어온다.
-                Object objectMember = ois.readObject();
-                member = (LazySingleton) objectMember;
-            }
-        }
-        return member;
+        return unSerialized;
     }
 
 }
