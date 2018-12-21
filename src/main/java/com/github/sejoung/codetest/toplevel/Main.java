e8af8dc1d86adc293392b54a3ddbabf6da9cfbe4
sejoung kim <sejoung@gmail.com>
2018-12-21 11:10:20 +0900

    클래스

---
+++ src/main/java/com/github/sejoung/codetest/toplevel/Main.java
@@ -0,0 +1,7 @@
+package com.github.sejoung.codetest.toplevel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
