f1437da60e5d92c7b142559895fb33cdfa795a1b
sejoung kim <sejoung@gmail.com>
2019-01-09 10:23:08 +0900

    ordinals 메서드 대신 instance fields를 사용하라

---
+++ src/main/java/com/github/sejoung/codetest/enumtest/EnsembleTest.java
@@ -0,0 +1,12 @@
+package com.github.sejoung.codetest.enumtest;
+
+public class EnsembleTest {
+
+    public static void main(String[] args) {
+
+        Ensemble ensemble=Ensemble.DECTET;
+
+        System.out.println(ensemble.numberOfMusicians());
+
+    }
+}
