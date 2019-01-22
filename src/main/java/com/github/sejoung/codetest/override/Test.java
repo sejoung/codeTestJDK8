9db3fa8609dd4ff6a3f318b4251eb6cecc108d9b
sejoung kim <sejoung@gmail.com>
2019-01-22 10:35:39 +0900

    코드

---
+++ src/main/java/com/github/sejoung/codetest/override/Test.java
@@ -0,0 +1,7 @@
+package com.github.sejoung.codetest.override;
+
+public abstract class Test {
+
+    public abstract void test();
+
+}
---
+++ src/main/java/com/github/sejoung/codetest/override/TestImpl.java
@@ -0,0 +1,14 @@
+package com.github.sejoung.codetest.override;
+
+public class TestImpl extends Test {
+
+    public void test(String a) {
+
+    }
+
+
+    @Override
+    public void test() {
+
+    }
+}
