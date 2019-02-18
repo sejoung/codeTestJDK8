8c7649cd4b101f9995ed2a60ca2271dc7c6a8d92
sejoung kim <sejoung@gmail.com>
2019-02-18 12:01:14 +0900

    아이템 52. 다중정의(overloading)는 신중히 사용하라.

---
+++ src/main/java/com/github/sejoung/codetest/methods/overloading/Lamda.java
@@ -0,0 +1,16 @@
+package com.github.sejoung.codetest.methods.overloading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Lamda {
+    public static void main(String[] args) {
+
+        new Thread(System.out::println).start();
+
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        es.submit(System.out::println);
+        String
+    }
+}
