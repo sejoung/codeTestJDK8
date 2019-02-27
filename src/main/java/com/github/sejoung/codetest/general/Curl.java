0544bc32b84ffff9c5d84e6fa275b8a82c0796bb
sejoung kim <sejoung@gmail.com>
2019-02-27 11:18:46 +0900

    아이템 59. 라이브러리를 익히고 사용하라.

---
+++ src/main/java/com/github/sejoung/codetest/general/Curl.java
@@ -0,0 +1,15 @@
+package com.github.sejoung.codetest.general;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+// 코드 59-2 transferTo 메서드를 이용해 URL의 내용 가져오기 - 자바 9부터 가능하다. (353쪽)
+public class Curl {
+    public static void main(String[] args) throws IOException {
+        try (InputStream in = new URL("https://www.naver.com").openStream()) {
+            in.transferTo(System.out);
+        }
+    }
+}
---
+++ src/main/java/com/github/sejoung/codetest/general/RandomBug.java
@@ -0,0 +1,24 @@
+package com.github.sejoung.codetest.general;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+// 무작위 수 생성은 쉽지 않다.
+public class RandomBug {
+    // 코드 59-1 흔하지만 문제가 심각한 코드! (351쪽)
+    static ThreadLocalRandom rnd = ThreadLocalRandom.current();
+
+    static int random(int n) {
+        return Math.abs(ThreadLocalRandom.current().nextInt()) % n;
+    }
+
+    // 무작위 수 1백만 개 생성 후, 중간 값보다 작은 수의 개수 출력 (351쪽)
+    public static void main(String[] args) {
+        int n = 2 * (Integer.MAX_VALUE / 3);
+        int low = 0;
+        for (int i = 0; i < 1000000; i++)
+            if (random(n) < n/2)
+                low++;
+        System.out.println(low);
+    }
+}
