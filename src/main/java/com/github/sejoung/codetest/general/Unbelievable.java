91a6416b177d44f0cd35f3fc87736eb46d46a86b
sejoung kim <sejoung@gmail.com>
2019-03-08 10:50:53 +0900

    아이템 61. 박싱된 기본타입 보단 기본타입을 사용하라.

---
+++ src/main/java/com/github/sejoung/codetest/general/Unbelievable.java
@@ -0,0 +1,12 @@
+package com.github.sejoung.codetest.general;
+
+// 코드 61-3 기이하게 동작하는 프로그램 - 결과를 맞혀보자! (360쪽)
+public class Unbelievable {
+//    static Integer i;
+    static int i;
+    public static void main(String[] args) {
+
+        if (i == 42)
+            System.out.println("믿을 수 없군!");
+    }
+}
