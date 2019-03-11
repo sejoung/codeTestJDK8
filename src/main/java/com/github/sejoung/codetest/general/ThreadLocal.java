02ba7d04e95d540f6469e9a61cdea8be96b66b76
sejoung kim <sejoung@gmail.com>
2019-03-11 10:32:49 +0900

    제네릭 사용 리팩토링

--- src/main/java/com/github/sejoung/codetest/general/ThreadLocal.java
+++ src/main/java/com/github/sejoung/codetest/general/ThreadLocal.java
@@ -1,16 +1,16 @@
 package com.github.sejoung.codetest.general;
 
-public class ThreadLocal {
+public class ThreadLocal<T> {
 
     public ThreadLocal() {
     }
 
     // 현 스레드의 값을 키로 구분해 저장
-    public void set(Object value) {
+    public void set(T value) {
     }
 
     //키가 가르키는 현 스레드의 값을 반환한다.
-    public  Object get() {
+    public  T get() {
         return null;
     }
 
