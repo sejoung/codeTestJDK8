588776eaaa5696eb0176dcf7152f5ed479c48153
sejoung kim <sejoung@gmail.com>
2019-03-25 12:23:15 +0900

    코드 추가

---
+++ src/main/java/com/github/sejoung/codetest/concurrent/synchronization/SynchronizedCounter.java
@@ -0,0 +1,18 @@
+package com.github.sejoung.codetest.concurrent.synchronization;
+
+public class SynchronizedCounter {
+
+  private int c = 0;
+
+  public synchronized void increment() {
+    c++;
+  }
+
+  public synchronized void decrement() {
+    c--;
+  }
+
+  public synchronized int value() {
+    return c;
+  }
+}
